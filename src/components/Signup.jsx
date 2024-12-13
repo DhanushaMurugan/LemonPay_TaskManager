9b1bb0ca26608c96b120ebb01959aefba13a16ed
DHANUSHA <mdhanusha7@gmail.com>
2024-12-13 12:19:31 +0530

    look & design alteration for signin page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { StopIcon } from "@heroicons/react/24/outline";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
@@ -16,11 +17,12 @@ const Signup = () => {
   return (
     <div className="flex justify-center items-center h-screen text-left w-96">
       <form onSubmit={handleSignup} className="p-8 w-96">
-        <h2 className="text-2xl font-bold mb-4">Welcome Login System</h2>
-        <p className="text-l">
-          Your gateway to seamless transactions and easy payments.
+        <h2 className="text-3xl mb-4">Welcome Login System</h2>
+        <p className="text-l mb-4">
+          Your gateway to seamless <br></br>transactions and easy payments.
         </p>
-        <label>Email</label>
+       <div className="mb-2"><label>Email</label></div>
+        
         <input
           type="email"
           placeholder="mahadev@lemonpay.tech"
@@ -29,15 +31,27 @@ const Signup = () => {
           required
           className="w-full p-2 mb-4 border border-gray-300 bg-customPurple  text-black rounded"
         />
-        <label>Password</label>
+        <div  className="mb-2"><label>Password</label></div>
+        
         <input
           type="password"
           placeholder="Min 8 characters"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-          className="w-full p-2 mb-4 border bg-white text-black rounded"
+          className="w-full p-2 mb-2 border bg-white text-black rounded"
         />
+        <div className="flex justify-between mb-6">
+          <div className="flex items-center">
+            <div><StopIcon class="h-6 w-6 text-white" /></div>
+          <div> <p>Remember me</p></div>
+
+           
+          </div>
+          <div>
+            <p>Forget Password?</p>
+          </div>
+        </div>
         <button
           type="submit"
           className="w-full bg-white text-black font-bold p-2 rounded"
